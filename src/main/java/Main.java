9c3af9329f61d6c097566ca7ed22d92d48a1ae96
Eduardo Tostado <eduardotostado@me.com>
2020-09-14 17:03:36 -0500

    Changes to the Log-In Screen

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("views/LogIn.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/views/LogIn.fxml"));
         primaryStage.setTitle("Fancy Picnics");
         primaryStage.setScene(new Scene(root, 600, 600));
         primaryStage.show();
